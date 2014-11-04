f497a17677b69b7374025c6ceff9b512d3e55f5e
Maxime Colmant <maxime.colmant@gmail.com>
2014-11-04 11:40:33 +0100

    add licence headers

--- build.sbt
+++ build.sbt
@@ -6,7 +6,8 @@ scalaVersion := "2.11.1"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.3.4",
-  "com.typesafe.akka" %% "akka-testkit" % "2.3.4",
+  "com.typesafe" % "config" % "1.2.1",
+  "com.typesafe.akka" %% "akka-testkit" % "2.3.4" % "test",
   "org.scalatest" %% "scalatest" % "2.1.6" % "test",
   "junit" % "junit" % "4.11" % "test",
   "com.novocode" % "junit-interface" % "0.10" % "test"
--- src/main/scala/org/powerapi/core/ClockActors.scala
+++ src/main/scala/org/powerapi/core/ClockActors.scala
@@ -1,3 +1,25 @@
+/**
+ * This software is licensed under the GNU Affero General Public License, quoted below.
+ *
+ * This file is a part of PowerAPI.
+ * 
+ * Copyright (C) 2011-2014 Inria, University of Lille 1.
+ * 
+ * PowerAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * PowerAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with PowerAPI.
+
+ * If not, please consult http://www.gnu.org/licenses/agpl-3.0.html.
+ */
 package org.powerapi.core
 
 import scala.collection._
@@ -31,7 +53,7 @@ class ClockChild(frequency: FiniteDuration) extends Component with ClockChannel 
   def running(acc: Int): Actor.Receive = {
     case msg: StopClock => stop(acc)
     case msg: StartClock => {
-      if(log.isDebugEnabled) log.debug(new StringContext("clock is already started, reference: ").s(frequency.toNanos))
+      if(log.isDebugEnabled) log.debug(new StringContext("clock is already started, reference: ", "").s(frequency.toNanos))
       sender ! NOK
       context.become(running(acc + 1))
     }
