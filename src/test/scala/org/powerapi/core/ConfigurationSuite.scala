7664b3ef3d669826a9bbefa7c044061b0fda51de
Maxime Colmant <maxime.colmant@gmail.com>
2014-11-17 13:18:52 +0100

    implement the configuration trait + tests

---
+++ src/test/scala/org/powerapi/core/ConfigurationSuite.scala
@@ -0,0 +1,80 @@
+/**
+ * This software is licensed under the GNU Affero General Public License, quoted below.
+ *
+ * This file is a part of PowerAPI.
+ * 
+ * Copyright (C) 2011-2014 Inria, University of Lille 1.
+ * 
+ * PowerAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * PowerAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with PowerAPI.
+
+ * If not, please consult http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package org.powerapi.core
+
+import org.powerapi.test.UnitTest
+
+import scala.collection.JavaConversions
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+
+import com.typesafe.config.{ Config, ConfigException }
+
+class ConfigurationMock extends Configuration {
+  val existingKey = load { _.getString("configuration-suite.key") }
+  val wrongKey = load { _.getString("configuration-suite.wrong-key") }
+  val map = load {
+    conf => {
+      (for(item: Config <- JavaConversions.asScalaBuffer(conf.getConfigList("configuration-suite.hash-map")))
+        yield (item.getString("key"), item.getString("value"))).toMap
+    }
+  }
+}
+
+
+class ConfigurationSuite(system: ActorSystem) extends UnitTest(system) {
+
+  def this() = this(ActorSystem("ConfigurationSuite"))
+
+  override def afterAll() = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  val config = new ConfigurationMock
+
+  "A ConfigurationMock class" should "read a value from a configuration file" in {
+    config.existingKey._1 match {
+      case Some(value) => value should equal("item")
+      case None => {}
+    }
+    config.existingKey._2 should equal(None)
+  }
+
+  it should "return the exception if the value asked does not exist" in {
+    config.wrongKey._1 should equal(None)
+    config.wrongKey._2 match {
+      case Some(ex) => ex shouldBe a [ConfigException]
+      case None => {}
+    }
+  }
+
+  it can "read complex values" in {
+    config.map._1 match {
+      case Some(map) => map should contain theSameElementsAs Map("item1" -> "value1", "item2" -> "value2")
+      case None => {}
+    }
+    config.map._2 should equal(None)
+  }
+}
