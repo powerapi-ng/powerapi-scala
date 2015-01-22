7d09bf863147e1a9d5d95c77c2a22caa12868733
huertas <l.huertas.pro@gmail.com>
2015-01-22 13:44:40 +0100

    feat(reporter): add new reporters
    add a file reporter
    add a jfreechart reporter

--- build.sbt
+++ build.sbt
@@ -10,7 +10,10 @@ libraryDependencies ++= Seq(
   "org.apache.logging.log4j" % "log4j-api" % "2.1",
   "org.apache.logging.log4j" % "log4j-core" % "2.1",
   "com.typesafe.akka" %% "akka-testkit" % "2.3.6" % "test",
-  "org.scalatest" %% "scalatest" % "2.2.2" % "test"
+  "org.scalatest" %% "scalatest" % "2.2.2" % "test",
+  "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.3",
+  "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.3",
+  "org.jfree" % "jfreechart" % "1.0.19"
 )
 
 scalacOptions ++= Seq(
---
+++ src/test/scala/org/powerapi/reporter/FileReporterSuite.scala
@@ -0,0 +1,98 @@
+/*
+ * This software is licensed under the GNU Affero General Public License, quoted below.
+ *
+ * This file is a part of PowerAPI.
+ *
+ * Copyright (C) 2011-2014 Inria, University of Lille 1.
+ *
+ * PowerAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * PowerAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with PowerAPI.
+ *
+ * If not, please consult http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+package org.powerapi.reporter
+
+import java.util.UUID
+
+import scala.concurrent.duration.DurationInt
+
+import akka.actor.{ ActorSystem, Props }
+import akka.testkit.{ TestActorRef, TestKit }
+import akka.util.Timeout
+
+import org.powerapi.UnitTest
+import org.powerapi.core.MessageBus
+import org.powerapi.module.PowerChannel.PowerReport
+
+object ConfigurationMock {
+  val testPath = "powerapi-reporter-file-test"
+}
+
+trait ConfigurationMock extends Configuration {
+  override lazy val filePath = ConfigurationMock.testPath
+}
+
+class FileReporterMock extends FileReporter with ConfigurationMock
+
+class FileReporterSpec(system: ActorSystem) extends UnitTest(system) {
+  implicit val timeout = Timeout(1.seconds)
+
+  def this() = this(ActorSystem("FileReporterSuite"))
+
+  override def afterAll() = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  val eventBus = new MessageBus
+  val reporterMock = TestActorRef(Props[FileReporterMock], "fileReporter")(system)
+
+  "A file reporter" should "process a power report and then report energy information in a file" in {
+    import scalax.file.Path
+    import org.powerapi.core.Process
+    import org.powerapi.core.ClockChannel.ClockTick
+    import org.powerapi.core.power._
+    import org.powerapi.module.PowerChannel.{ AggregateReport, RawPowerReport, render, subscribeAggPowerReport }
+    
+    val muid = UUID.randomUUID()
+    val device = "mock"
+    val tickMock = ClockTick("ticktest", 25.milliseconds)
+    val aggFunction = (s: Seq[Power]) => s.foldLeft(0.0.W){ (acc, p) => acc + p }
+  
+    subscribeAggPowerReport(muid)(eventBus)(reporterMock)
+    
+    val aggR1 = AggregateReport(muid, aggFunction)
+    aggR1 += RawPowerReport("topictest", muid, Process(1), 3.W, device, tickMock)
+    
+    val aggR2 = AggregateReport(muid, aggFunction)
+    aggR2 += RawPowerReport("topictest", muid, Process(2), 1.W, device, tickMock)
+    
+    val aggR3 = AggregateReport(muid, aggFunction)
+    aggR3 += RawPowerReport("topictest", muid, Process(3), 2.W, device, tickMock)
+    
+    render(aggR1)(eventBus)
+    render(aggR2)(eventBus)
+    render(aggR3)(eventBus)
+
+    val testFile = Path.fromString(ConfigurationMock.testPath)
+    testFile.isFile should be (true)
+    testFile.size.get should be > 0L
+    testFile.lines() should (
+      have size 3 and
+      contain(LineMock(RawPowerReport("topictest", muid, Process(1), 3.W, device, tickMock)).toString) and 
+      contain(LineMock(RawPowerReport("topictest", muid, Process(2), 1.W, device, tickMock)).toString) and
+      contain(LineMock(RawPowerReport("topictest", muid, Process(3), 2.W, device, tickMock)).toString)
+    )
+    testFile.delete(true)
+  }
+}
+
