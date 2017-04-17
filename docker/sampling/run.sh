9c584f99cdf3b896a4d2a9f1b20577da3b650cbb
Maxime Colmant <maxime.colmant@gmail.com>
2017-04-17 20:27:59 +0200

    fix: travis build

--- docker/sampling/run.sh
+++ docker/sampling/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd powerapi-sampling && ./bin/sampling --all results/sampling results/processing results/computing
+cd powerapi-sampling-cpu && ./bin/sampling-cpu --all results/sampling results/processing results/computing
 
 echo ""
 echo "Here is your CPU power model to use with PowerAPI"
