9be1f24ecba4327b8d0f8db61c041f49aeaf7462
gfieni <gfieni@users.noreply.github.com>
2018-02-19 15:57:00 +0100

    refactor(Docker): Rework of cli and cpu-sampling Docker images
    Using Ubuntu Xenial (LTS) as base to benefit from stable packages instead of compiling dependencies.

--- docker/sampling/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd powerapi-sampling-cpu && ./bin/sampling-cpu --all results/sampling results/processing results/computing
-
-echo ""
-echo "Here is your CPU power model to use with PowerAPI"
-echo ""
-cat results/computing/libpfm-formula.conf
-echo ""
